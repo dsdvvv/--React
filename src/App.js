a0416cd7b59ac11a51a77179cab450100b25b387
dsdvvv <2473378640@qq.com>
2020-01-02 17:31:39 +0800

    首次提交-项目创建及数据部署

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// 根组件: 使用其他组件
+import React from 'react';
+
+class App extends React.Component {
+  componentDidMount () {
+
+  }
+  render () {
+    return (
+      <div>App</div>
+    )
+  }
+}
+export default App;
